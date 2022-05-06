b0c5b2831043b6deefb20fe27d981274c2d8656e
Iwuchkwu O <kizob123@gmail.com>
2022-05-06 19:57:50 +0100

    21th commit: Overhaul

--- fourfourplace/src/index.js
+++ fourfourplace/src/index.js
@@ -13,9 +13,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <App/>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
  
        </>
